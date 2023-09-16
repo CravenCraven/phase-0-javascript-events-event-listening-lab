c898dd49f7b09f1c99485c8959d67a48107787ff
nicolecraven <craventhegreat@gmail.com>
2023-09-15 22:15:14 -0400

    f

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function handleEvent(event) {
     console.log('Event was triggered:', event.type);
 }
 element.addEventListener('click', handleEvent);
+const button = document.getElementById('myButton');
+function handleClick() {
+    alert('Button Clicked!');
+}
+button.addEventListener('click', handleClick);
