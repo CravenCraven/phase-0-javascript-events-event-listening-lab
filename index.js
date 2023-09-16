8437b8edd699512a11b75d6179f7defa366c0091
nicolecraven <craventhegreat@gmail.com>
2023-09-15 22:10:35 -0400

    failed

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
 }
+const element = document.getElementById('myElement');
+function handleEvent(event) {
+    console.log('Event was triggered:', event.type);
+}
+element.addEventListener('click', handleEvent);
